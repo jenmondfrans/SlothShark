2c6a735d7f000a45ebada0da60f6203b7d0f4b4b
Mercedes Coyle <mcoyle@realgravity.com>
2013-12-14 20:23:30 -0800

    added flex sensor monitoring code

--- SlothSharkSleepingEYES/SlothSharkSleepingEYES.ino
+++ SlothSharkSleepingEYES/SlothSharkSleepingEYES.ino
@@ -2,7 +2,10 @@
 // a sleeping computer
 
 const int LED = 13;  // the pin for the standard LED
-int i = 0;          // We'll use this to count up and down
+int i = 0;           // We'll use this to count up and down
+int flex_sensor_pin = A0;      // analog input pin for flex sensor input
+int begin_loop = 0;
+int flex_value = 0;
 int left_eye[3] = {9, 10, 11}; // arrays control our led rgb pins
 int right_eye[3] = {3, 5, 6};  // to keep the code a little simpler
 // green: pin 9
@@ -10,31 +13,38 @@ int right_eye[3] = {3, 5, 6};  // to keep the code a little simpler
 // blue:  pin 11
 
 void setup() {
+    attachInterrupt(flex_sensor_pin, detect_flex, RISING);
     pinMode(left_eye[0], OUTPUT);
     pinMode(left_eye[1], OUTPUT);
     pinMode(left_eye[2], OUTPUT);
     pinMode(right_eye[0], OUTPUT);
     pinMode(right_eye[1], OUTPUT);
     pinMode(right_eye[2], OUTPUT);
-  // for (i = 0; i < 4; i++) {
-    // pinMode(left_eye[i], OUTPUT); // tell Arduino left eye is an output
-    // pinMode(right_eye[i], OUTPUT); // tell Arduino right eye is an output
-  // }
+    pinMode(LED, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  nukular_eyes_on();
+  detect_flex();
   delay(1000);
-  nukular_eyes_off();
-  delay(1000);
-  rainbow_eyes();
-  delay(1000);
-  sleepy_eyes();
-  delay(1000);
+  // begin_loop = micros();
+  // detect_flex();
+  // delay(1000);
+  // wait for flex sensor input to call these functions
+  // nukular_eyes_on();
+  // delay(1000);
+  // nukular_eyes_off();
+  // delay(1000);
+  // rainbow_eyes();
+  // delay(1000);
+  // sleepy_eyes();
+  // delay(1000);
 }
 // end of main loop                                     
 
 void detect_flex() {
+  flex_value = analogRead(flex_sensor_pin);
+  Serial.println(flex_value);
   // this function will handle our flex sensor input
 }
 
@@ -69,10 +79,10 @@ void rainbow_eyes() {
   } 
 }
 void sleepy_eyes() {
-  for (i = 0; i <= 255; i++) {  // loop from 0 to 254 (fade in)
-    analogWrite(left_eye[2], i);       // set the LED brightness
+  for (i = 0; i <= 255; i++) {    // loop from 0 to 254 (fade in)
+    analogWrite(left_eye[2], i);  // set the LED brightness
     analogWrite(right_eye[2], i);
-    delay(5);  // Wait 10ms because analogWrite
+    delay(5);   // Wait (n)ms because analogWrite
                 // is instantaneous and we would
                 // not see any change
   }
